951a9cd571dccb1058f8c2d9dd1f1ea8f8852141
ciacono <claire.iacono@gmail.com>
2019-02-09 22:16:04 -0800

    Implemented ShoppingHistory

--- src/models/ShoppingHistory.java
+++ src/models/ShoppingHistory.java
@@ -1,13 +1,44 @@
 package models;
 
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
 
 public class ShoppingHistory implements Observer{
 
-//comment
+    private HashMap<Status, List<FoodManager>> historyMap = new HashMap<>();
+
+    public ShoppingHistory() {
+        this.historyMap.put(Status.NEW, new ArrayList<>());
+        this.historyMap.put(Status.SOON, new ArrayList<>());
+        this.historyMap.put(Status.EXPIRED, new ArrayList<>());
+        this.historyMap.put(Status.EATEN, new ArrayList<>());
+        this.historyMap.put(Status.THROWNOUT, new ArrayList<>());
+    }
+
+    //TODO: INTEGRATE OBSERVER PATTERN AND FRONT-END
+    private void addItem(FoodManager newItem){
+        List<FoodManager> foodList = historyMap.get(newItem.getStatus());
+        foodList.add(newItem);
+    }
+
+    //TODO: integrate with observer pattern & front-end
+    private void removeItem(FoodManager foodItem) {
+        List<FoodManager> foodList = historyMap.get(foodItem.getStatus());
+        if (foodList.contains(foodItem)) {
+            foodList.remove(foodItem);
+        }
+    }
+
     @Override
     public void update(Observable o, Object arg) {
+        FoodManager foodItem = (FoodManager) o;
+        Status status = (Status) arg;
+        changeFoodStatus(foodItem, status);
+    }
 
+    private void changeFoodStatus(FoodManager foodItem, Status status){
+        Status currStatus = foodItem.status;
+        historyMap.remove(currStatus, foodItem);
+        List<FoodManager> tempList =  historyMap.get(status);
+        tempList.add(foodItem);
     }
 }
